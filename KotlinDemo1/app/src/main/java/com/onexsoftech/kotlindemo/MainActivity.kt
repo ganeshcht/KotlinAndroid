f635b55dba55330dbe49c8edffe003a1b03d3627
Ganesh C <ganeshch@onexsoftech.com>
2020-04-01 14:10:53 +0530

    Example of Button Click Listener and showing simple text Message.

--- KotlinDemo1/app/src/main/java/com/onexsoftech/kotlindemo/MainActivity.kt
+++ KotlinDemo1/app/src/main/java/com/onexsoftech/kotlindemo/MainActivity.kt
@@ -2,11 +2,18 @@ package com.onexsoftech.kotlindemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.* // this import for directly use view id's in business code
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //here i use btnClickMsg which is id of Button view in activity_main.xml file
+        //here i directly use setOnClickListener method of Button View
+        btnClickMsg.setOnClickListener {
+            Toast.makeText(this,"I am Clicked!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
