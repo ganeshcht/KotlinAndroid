282191a5a17c2aa52eeb94fd0b5a4ea4e7592415
Ganesh C <ganeshch@onexsoftech.com>
2020-04-04 15:22:57 +0530

    Example 3

--- KotlinDemo1/app/src/main/java/com/onexsoftech/kotlindemo/MainActivity.kt
+++ KotlinDemo1/app/src/main/java/com/onexsoftech/kotlindemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.onexsoftech.kotlindemo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -10,18 +11,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //******************First Example***********************//
         //here i use btnClickMsg which is id of Button view in activity_main.xml file
         //here i directly use setOnClickListener method of Button View
         btnClick.setOnClickListener {
             Toast.makeText(this,"I am Clicked!",Toast.LENGTH_SHORT).show()
         }
-
+        //********************Second Exmaple*********************//
         //Here we are getting value from edit text and displaying it on Toast message
         //first we get text value from edit text (ed_message)
-        val msg=ed_message.text.toString()  //in kotlin there is no concept of getter/setter
+        //val msg=ed_message.text.toString()  //in kotlin there is no concept of getter/setter
         //On Show Message button click we showing message typed on EditText
-        btnShowMsg.setOnClickListener {
+        /*btnShowMsg.setOnClickListener {
             Toast.makeText(this,"Your message:$msg",Toast.LENGTH_LONG).show()
+        }*/
+
+        //*********************third Exmple**********************//
+        //Here we are getting value from edit text and passing that value to next Activity using intent
+        //first we get string valur from edit text(ed_message)
+        val msg=ed_message.text.toString()
+        btnShowMsg.setOnClickListener {
+            val intent=Intent(this,SecondActivity::class.java)// here in second parameter we use kotlin Reflection
+            intent.putExtra("message",msg)
+            startActivity(intent)
         }
     }
 }
---
+++ KotlinDemo1/app/src/main/java/com/onexsoftech/kotlindemo/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.onexsoftech.kotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        //in the second Activity we get the message string from Main Activity and Simply displayed as Toast
+        //first we get the not nullable Bundle
+        val bundle:Bundle?=intent.extras   //As intent.extras method gives Nullable object. So to avoid null pointer Exception in kotlin use '?'
+        val msg=bundle!!.getString("message") // As bundle is nullable here use '!!' assertion operator
+        Toast.makeText(this,"$msg",Toast.LENGTH_SHORT).show()
+    }
+}
